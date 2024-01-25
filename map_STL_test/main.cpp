ddc81c95e8d273e7912611bb3ac411dda78cd0cd
zbjoy <664266432@qq.com>
2024-01-25 17:20:51 +0800

    仿函数还是不会使用,set容器不能插入自己写的类的对象

---
+++ map_STL_test/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+class Maker;
+class makerFunc;
+void printSet(set<Maker, makerFunc>& s);
+
+class Maker
+{
+    friend ostream& operator<<(ostream& out, Maker& m);
+    friend void printSet(set<Maker, makerFunc>& s);
+public:
+    Maker(){};
+    Maker(string tName, int tAge) : name(tName), age(tAge){};
+    string getName()
+    {
+        return name;
+    }
+
+    int getAge()
+    {
+        return age;
+    }
+private:
+    string name;
+    int age;
+};
+
+ostream& operator<<(ostream& out, Maker& m)
+{
+    out << "Name: " << m.name << " Age: " << m.age;
+    return out;
+}
+
+class makerFunc
+{
+public:
+    bool operator()(Maker& m1, Maker& m2)
+    {
+        return m1.getAge() > m2.getAge();
+    }
+};
+
+//template <typename T>
+void printSet(set<Maker, makerFunc>& s)
+{
+    for(set<Maker, makerFunc>::iterator it = s.begin(); it != s.end(); it++)
+    {
+        
+        // cout << (*it) << endl;
+        cout << "Name: " << it->name << " Age: " << it->age << endl;
+    }
+}
+
+
+
+void test()
+{
+    set<Maker, makerFunc> s;
+    makerFunc make;
+    //make(Maker("aaa", 10), Maker("bbb", 20));
+    Maker m1("aaa", 10);
+    Maker m2("bbb", 20);
+    make(m1, m2);
+    //makerFunc(Maker("aaa", 10), Maker("bbb", 20));
+    //s.insert(m1);//Maker("aaa", 10)); // error !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! insert失败
+    // s.insert(Maker("bbb", 20));
+    // s.insert(Maker("ccc", 30));
+    // s.insert(Maker("ddd", 40));
+    // s.insert(Maker("eee", 50));
+
+    //printSet(s);
+}
+
+void test02()
+{
+    map<int, Maker> mymap;
+    mymap.insert(make_pair(2, Maker("aaa", 10)));
+    Maker m2("bbb", 20);
+    // mymap[6] = Maker("ccc", 30); // error
+    mymap[6] = m2;
+    for(map<int, Maker>::iterator it = mymap.begin(); it != mymap.end(); it++)
+    {
+        cout << it->second.getName() << "   " << it->second.getAge() << endl;
+    }
+}
+
+int main()
+{
+    //test();//err ?????????
+    test02();
+    return 0;
+}
